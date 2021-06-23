5f2d21c3853ffe2aae85037f15c0a128da06d90c
Akash <gsaisriram.akash@gmail.com>
2021-06-23 21:25:33 +0530

    Updates

---
+++ precache-manifest.390512d627046a8af3c91bcad658c410.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f8929319ee39f2e9f83",
+    "url": "/ImageCrypt/static/css/main.65b06096.chunk.css"
+  },
+  {
+    "revision": "3f8929319ee39f2e9f83",
+    "url": "/ImageCrypt/static/js/main.3f892931.chunk.js"
+  },
+  {
+    "revision": "5d2a4bba2db2974eaee6",
+    "url": "/ImageCrypt/static/css/1.9d2844f2.chunk.css"
+  },
+  {
+    "revision": "5d2a4bba2db2974eaee6",
+    "url": "/ImageCrypt/static/js/1.5d2a4bba.chunk.js"
+  },
+  {
+    "revision": "c30c6710a3765a642cdf",
+    "url": "/ImageCrypt/static/js/runtime~main.c30c6710.js"
+  },
+  {
+    "revision": "f24b9975dda447580d081c2e529bd2a1",
+    "url": "/ImageCrypt/index.html"
+  }
+];
